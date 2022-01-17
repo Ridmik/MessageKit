c03b3beb4387a6567f1acb2a350c82bcf4affc73
Muhtasim Ulfat tanmoy <mtanmoy5086@gmail.com>
2022-01-17 12:39:45 +0600

    RPLUS-2255: Fix scroll when keyboard open

--- Sources/Controllers/MessagesViewController+Keyboard.swift
+++ Sources/Controllers/MessagesViewController+Keyboard.swift
@@ -99,6 +99,11 @@ internal extension MessagesViewController {
 
         if maintainPositionOnKeyboardFrameChanged && differenceOfBottomInset != 0 {
             let contentOffset = CGPoint(x: messagesCollectionView.contentOffset.x, y: messagesCollectionView.contentOffset.y + differenceOfBottomInset)
+            
+            guard contentOffset.y <= messagesCollectionView.contentSize.height else {
+                 return
+            }
+
             messagesCollectionView.setContentOffset(contentOffset, animated: true)
         }
 
